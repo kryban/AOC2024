4d6091cd605bfa5b8b1f669a6c25cc43cc49375c
Karoy Bandi <BandiK@wigo4it.nl>
2024-12-13 11:58:41 +0100

    addes day 1 part 2 solution

--- AdventOfCode/Day01/Program.cs
+++ AdventOfCode/Day01/Program.cs
@@ -9,16 +9,38 @@ class Program
         //read input
         string input = System.IO.File.ReadAllText("input.txt");
         List<int> inputList = input.Split(",").ToList().Select(int.Parse).ToList();
-        int answer = 0;
-        int index = 0;
+        
         List<int> right = inputList.Where((item, index) => index % 2 != 0).Order().ToList();
         List<int> left = inputList.Where((item, index) => index % 2 == 0).Order().ToList();
+
+        PartOne(left, right);
+        PartTwo(left, right);
+    }
+
+    private static void PartOne(List<int> left, List<int> right)
+    {
+        int answer = 0;
+        int i = 0;
         
         foreach (int leftitem in left)
         {
-             answer = answer + Math.Abs(leftitem - right[index++]);;
+            //calculate difference and add to answer
+            answer = answer + Math.Abs(leftitem - right[i++]);;
         }
         
-        Console.WriteLine(answer); //1590491
+        Console.WriteLine("Day1 - Part1: {0}",answer); //1590491
+    }
+    
+    private static void PartTwo(List<int> left, List<int> right)
+    {
+        int answer = 0;
+        
+        foreach (int leftitem in left)
+        {
+            //calculate difference and add to answer
+            answer = answer + (leftitem * right.Count(x => x == leftitem));
+        }
+        
+        Console.WriteLine("Day1 - Part2: {0}",answer); //
     }
 }
