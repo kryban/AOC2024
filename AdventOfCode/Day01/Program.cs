f91abe82bfd202f02f1646bc8d787bce9ce103bc
Karoy Bandi <BandiK@wigo4it.nl>
2024-12-13 06:12:47 +0100

    Answer

--- AdventOfCode/Day01/Program.cs
+++ AdventOfCode/Day01/Program.cs
@@ -1,9 +1,24 @@
-﻿namespace Day01;
+﻿using System.Text.RegularExpressions;
+
+namespace Day01;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, Worlddd!");
+        //read input
+        string input = System.IO.File.ReadAllText("input.txt");
+        List<int> inputList = input.Split(",").ToList().Select(int.Parse).ToList();
+        int answer = 0;
+        int index = 0;
+        List<int> right = inputList.Where((item, index) => index % 2 != 0).Order().ToList();
+        List<int> left = inputList.Where((item, index) => index % 2 == 0).Order().ToList();
+        
+        foreach (int leftitem in left)
+        {
+             answer = answer + Math.Abs(leftitem - right[index++]);;
+        }
+        
+        Console.WriteLine(answer); //1590491
     }
 }
