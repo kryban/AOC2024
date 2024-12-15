fc414aa4d6f28b2c755d59c01683a5930c980326
kryban <kry.ban@gmail.com>
2024-12-15 12:44:23 +0100

    ontbrekende bestanden toevoegen

---
+++ AdventOfCode/Day03/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Text.RegularExpressions;
+
+string input = File.ReadAllText("input.txt");
+
+Part1(input);
+
+void Part1(string s)
+{
+    int antwoord = 0;
+    var mulSelectie = @"mul\(\b\d{1,3}\b,\b\d{1,3}\b\)";
+    var getallenSelectie = @"\b\d{1,3}\b,\b\d{1,3}\b";
+
+    MatchCollection matches = Regex.Matches(s, mulSelectie);
+
+    matches.Select(match => match.Value).ToList().ForEach((value) =>
+    {
+        VoegToeAanAntwoord(value, ref antwoord);
+    });
+
+    void VoegToeAanAntwoord(string value, ref int antw)
+    {
+        var getallen = Regex.Match(value, getallenSelectie)
+            .Value
+            .Split(",");
+    
+        var getal = int.Parse(getallen[0]) * int.Parse(getallen[1]);
+
+        antw += getal;
+    }
+
+    Console.WriteLine("Day 1 - Part1: {0}", antwoord.ToString()); //167090022
+}
