9bacfd1d5c0cbf89de5fda3f5c4a3a80e2010ab5
kryban <kry.ban@gmail.com>
2024-12-16 12:59:51 +0100

    wip

--- AdventOfCode/Day03/Program.cs
+++ AdventOfCode/Day03/Program.cs
@@ -1,8 +1,12 @@
 ﻿using System.Text.RegularExpressions;
 
-string input = File.ReadAllText("input.txt");
+string input = File.ReadAllText("voorbeeldPart2.txt");
+
+List<string> dos = [];
+List<string> donts = [];
 
 Part1(input);
+Part2(input); 
 
 void Part1(string s)
 {
@@ -28,5 +32,21 @@ void Part1(string s)
         antw += getal;
     }
 
-    Console.WriteLine("Day 1 - Part1: {0}", antwoord.ToString()); //167090022
+    Console.WriteLine("Day 3 - Part1: {0}", antwoord.ToString()); //167090022
 }
+
+void Part2(string s)
+{
+    
+    //(?<=don't\(\)).*
+    //xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))dddddon't())))))
+    int antwoord = 0;
+    var mulSelectie = @"mul\(\b\d{1,3}\b,\b\d{1,3}\b\)";
+    var getallenSelectie = @"\b\d{1,3}\b,\b\d{1,3}\b";
+    var doOrDont = @"don't\(\)|do\(\)";
+    
+    var doList = s.Split("do()");
+    
+    Console.WriteLine("Day 3 - Part2: {0}", antwoord.ToString()); //
+}
+
