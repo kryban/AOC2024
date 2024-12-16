e5f6ce956d936ceb2ae0f73ec0d932e49ec845c4
Karoy Bandi <BandiK@wigo4it.nl>
2024-12-16 16:01:32 +0100

    wip voorbeeld

--- AdventOfCode/Day03/Program.cs
+++ AdventOfCode/Day03/Program.cs
@@ -5,7 +5,7 @@ string input = File.ReadAllText("voorbeeldPart2.txt");
 List<string> dos = [];
 List<string> donts = [];
 
-Part1(input);
+//Part1(input);
 Part2(input); 
 
 void Part1(string s)
@@ -39,14 +39,47 @@ void Part2(string s)
 {
     
     //(?<=don't\(\)).*
-    //xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))dddddon't())))))
+        // breek totale string op in substrings tot do() of don'ts()
+        //xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?don't()mul(9,5))aaaaaamul(8,5)dddd don't())))))foo
+        //----
+        //xmul(2,4)&mul[3,7]!^ -->? selecteer muil
+        //don't()_mul(5,5)+mul(32,64](mul(11,8)un
+        //do()?
+        //don't()mul(8,5))aaaaaamul(8,5)dddd
+        //don't())))))
     int antwoord = 0;
     var mulSelectie = @"mul\(\b\d{1,3}\b,\b\d{1,3}\b\)";
     var getallenSelectie = @"\b\d{1,3}\b,\b\d{1,3}\b";
-    var doOrDont = @"don't\(\)|do\(\)";
+    var doOrDont = @"(don't\(\)|do\(\))";
     
-    var doList = s.Split("do()");
+    List<string> doList = [];
+    List<string> dontList = [];
+    bool naarDoList = true;
+    
+    var splittedSubs = Regex.Split(s, doOrDont);
+    
+    foreach (var sub in splittedSubs)
+    {
+        DoOrNotDo(sub, ref naarDoList);
+        
+        if (naarDoList)
+        { 
+            doList.Add(sub);
+        }
+        else
+        {
+            dontList.Add(sub);
+        }
+    }
     
     Console.WriteLine("Day 3 - Part2: {0}", antwoord.ToString()); //
 }
 
+static void DoOrNotDo(string input, ref bool naarDoList)
+{
+    if(input.Contains(@"do()"))
+        naarDoList = true;
+    
+    if(input.Contains(@"don't()"))
+        naarDoList = false;
+}
