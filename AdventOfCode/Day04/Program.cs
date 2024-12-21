e24799004e5bd1feb2df738005ccb48cff25d1ad
kryban <kry.ban@gmail.com>
2024-12-21 09:43:40 +0100

    Day 4 - part 2 solution

--- AdventOfCode/Day04/Program.cs
+++ AdventOfCode/Day04/Program.cs
@@ -3,6 +3,7 @@ string vooruit = "XMAS";
 string achteruit = "SAMX";
 
 Part1(input); //2603
+Part2(input); //1965
 
 void Part1(List<string> inp)
 {
@@ -15,171 +16,235 @@ void Part1(List<string> inp)
         antwoord += VindHorizontaal(line, achteruit, huidigeRij);
         antwoord += VindVerticaal(line, inp, huidigeRij, vooruit);
         antwoord += VindVerticaal(line, inp, huidigeRij, achteruit);
-        antwoord += VindDiagonaalRechts(line, inp,huidigeRij, vooruit);
-        antwoord += VindDiagonaalRechts(line, inp,huidigeRij, achteruit);
-        antwoord += VindDiagonaalLinks(line, inp,huidigeRij, vooruit);
-        antwoord += VindDiagonaalLinks(line, inp,huidigeRij, achteruit);
-        
+        antwoord += VindDiagonaalRechts(line, inp, huidigeRij, vooruit);
+        antwoord += VindDiagonaalRechts(line, inp, huidigeRij, achteruit);
+        antwoord += VindDiagonaalLinks(line, inp, huidigeRij, vooruit);
+        antwoord += VindDiagonaalLinks(line, inp, huidigeRij, achteruit);
+
         huidigeRij++;
     }
-    
-    Console.WriteLine($"Day 4 - Part 1: {antwoord}");
-}
 
-int VindHorizontaal(string line, string zoekwoord, int huidigeRij)
-{
-    int startpositie = 0;
-    int matchAantal = 0;
-    int retval = 0;
-    
-    while ((startpositie + zoekwoord.Length) <= line.Length)
+    Console.WriteLine($"Day 4 - Part 1: {antwoord}"); //2603
+
+    int VindHorizontaal(string line, string zoekwoord, int huidigeRij)
     {
-        matchAantal = MatchesHorizontaal(line, zoekwoord, startpositie);
+        int startpositie = 0;
+        int matchAantal = 0;
+        int retval = 0;
 
-        retval += matchAantal == zoekwoord.Length ? 1 : 0;
-        startpositie++;
-    }
 
-    return retval;
-}
-
-int MatchesHorizontaal(string zoeklijn, string zoekwoord, int startpositie)
-{
-    int zoekpositie = startpositie;
-    var retval = 0;
-    
-    foreach (var character in zoekwoord)
-    {
-        char charInFocusVoorVergelijking = zoeklijn[zoekpositie];
-            
-        if(charInFocusVoorVergelijking.Equals(character))
+        while ((startpositie + zoekwoord.Length) <= line.Length)
         {
-            retval++;
-        }
-            
-        zoekpositie++;
-    }
+            matchAantal = MatchesHorizontaal(line, zoekwoord, startpositie);
 
-    return retval;
-}
-
-int VindVerticaal(string s, List<string> lijstMetInputRegels, int huidigeRij, string zoekwoord)
-{
-    int startpositie = 0;
-    int matchAantal = 0;
-    int retval = 0;
-    
-    while (startpositie < s.Length && (huidigeRij+zoekwoord.Length <= lijstMetInputRegels.Count))
-    {
-        matchAantal = AantalMatchesVanZoekwoordVerticaal(huidigeRij, zoekwoord, lijstMetInputRegels, startpositie);
-
-        retval += matchAantal == zoekwoord.Length ? 1 : 0;
-        startpositie++;
-    }
-
-    return retval;
-}
-
-int VindDiagonaalRechts(string inputRegel, List<string> lijstMetInputRegels, int huidigeRij, string zoekwoord)
-{
-    int startpositie = 0;
-    int matchAantal = 0;
-    int retval = 0;
-    int zoekrichtingRechts = 1;
-
-    bool ZoekwoordPastHorizontaalRechts()
-    {
-        return (startpositie + zoekwoord.Length) <= inputRegel.Length;
-    }
-
-    bool ZoekwoordPastVerticaal()
-    {
-        return huidigeRij+zoekwoord.Length <= lijstMetInputRegels.Count;
-    }
-
-    bool ZoekwoordPastGeheelInInputRegels()
-    {
-        return ZoekwoordPastHorizontaalRechts() && ZoekwoordPastVerticaal();
-    }
-
-    while (ZoekwoordPastGeheelInInputRegels())
-    {
-        matchAantal = AantalMatchesVanZoekwoordDiagonaal(huidigeRij, zoekwoord, lijstMetInputRegels, startpositie, zoekrichtingRechts);
-
-        retval += matchAantal == zoekwoord.Length ? 1 : 0;
-        startpositie++;
-    }
-
-    return retval;
-}
-
-int VindDiagonaalLinks(string inputRegel, List<string> lijstMetInputRegels, int huidigeRij, string zoekwoord)
-{
-    int startpositie = 0;
-    int matchAantal = 0;
-    int retval = 0;
-    int zoekrichtingRechts = -1;
-
-    bool ZoekwoordPastHorizontaalLinks()
-    {
-        return (startpositie - (zoekwoord.Length-1)) >= 0;
-    }
-
-    bool ZoekwoordPastVerticaal()
-    {
-        return huidigeRij+zoekwoord.Length <= lijstMetInputRegels.Count;
-    }
-
-    bool ZoekwoordPastGeheelInInputRegels()
-    {
-        return ZoekwoordPastHorizontaalLinks() && ZoekwoordPastVerticaal();
-    }
-
-    while (startpositie < inputRegel.Length && ZoekwoordPastVerticaal())
-    {
-        if (!ZoekwoordPastHorizontaalLinks())
-        {
+            retval += matchAantal == zoekwoord.Length ? 1 : 0;
             startpositie++;
-            continue;
+        }
+
+        return retval;
+    }
+
+    int MatchesHorizontaal(string zoeklijn, string zoekwoord, int startpositie)
+    {
+        int zoekpositie = startpositie;
+        var retval = 0;
+
+        foreach (var character in zoekwoord)
+        {
+            char charInFocusVoorVergelijking = zoeklijn[zoekpositie];
+
+            if (charInFocusVoorVergelijking.Equals(character))
+            {
+                retval++;
+            }
+
+            zoekpositie++;
+        }
+
+        return retval;
+    }
+
+    int VindVerticaal(string s, List<string> lijstMetInputRegels, int huidigeRij, string zoekwoord)
+    {
+        int startpositie = 0;
+        int matchAantal = 0;
+        int retval = 0;
+
+        while (startpositie < s.Length && (huidigeRij + zoekwoord.Length <= lijstMetInputRegels.Count))
+        {
+            matchAantal = AantalMatchesVanZoekwoordVerticaal(huidigeRij, zoekwoord, lijstMetInputRegels, startpositie);
+
+            retval += matchAantal == zoekwoord.Length ? 1 : 0;
+            startpositie++;
+        }
+
+        return retval;
+    }
+
+    int VindDiagonaalRechts(string inputRegel, List<string> lijstMetInputRegels, int huidigeRij, string zoekwoord)
+    {
+        int startpositie = 0;
+        int matchAantal = 0;
+        int retval = 0;
+        int zoekrichtingRechts = 1;
+
+        bool ZoekwoordPastHorizontaalRechts()
+        {
+            return (startpositie + zoekwoord.Length) <= inputRegel.Length;
+        }
+
+        bool ZoekwoordPastVerticaal()
+        {
+            return huidigeRij + zoekwoord.Length <= lijstMetInputRegels.Count;
+        }
+
+        bool ZoekwoordPastGeheelInInputRegels()
+        {
+            return ZoekwoordPastHorizontaalRechts() && ZoekwoordPastVerticaal();
+        }
+
+        while (ZoekwoordPastGeheelInInputRegels())
+        {
+            matchAantal = AantalMatchesVanZoekwoordDiagonaal(huidigeRij, zoekwoord, lijstMetInputRegels, startpositie,
+                zoekrichtingRechts);
+
+            retval += matchAantal == zoekwoord.Length ? 1 : 0;
+            startpositie++;
+        }
+
+        return retval;
+    }
+
+    int VindDiagonaalLinks(string inputRegel, List<string> lijstMetInputRegels, int huidigeRij, string zoekwoord)
+    {
+        int startpositie = 0;
+        int matchAantal = 0;
+        int retval = 0;
+        int zoekrichtingRechts = -1;
+
+        bool ZoekwoordPastHorizontaalLinks()
+        {
+            return (startpositie - (zoekwoord.Length - 1)) >= 0;
+        }
+
+        bool ZoekwoordPastVerticaal()
+        {
+            return huidigeRij + zoekwoord.Length <= lijstMetInputRegels.Count;
+        }
+
+        bool ZoekwoordPastGeheelInInputRegels()
+        {
+            return ZoekwoordPastHorizontaalLinks() && ZoekwoordPastVerticaal();
+        }
+
+        while (startpositie < inputRegel.Length && ZoekwoordPastVerticaal())
+        {
+            if (!ZoekwoordPastHorizontaalLinks())
+            {
+                startpositie++;
+                continue;
+            }
+
+            matchAantal = AantalMatchesVanZoekwoordDiagonaal(huidigeRij, zoekwoord, lijstMetInputRegels, startpositie,
+                zoekrichtingRechts);
+
+            retval += matchAantal == zoekwoord.Length ? 1 : 0;
+            startpositie++;
+        }
+
+        return retval;
+    }
+
+    int AantalMatchesVanZoekwoordDiagonaal(int huidigeRij, string zoekwoord, List<string> lijstMetInputRegels,
+        int startpositie, int zoekrichting)
+    {
+        int zoekrij = huidigeRij;
+        return ZoekCharacterMatchInOnderliggendRij(zoekwoord, lijstMetInputRegels, zoekrij, startpositie, zoekrichting);
+    }
+
+    int AantalMatchesVanZoekwoordVerticaal(int huidigeRij, string zoekwoord, List<string> lijstMetInputRegels,
+        int startpositie)
+    {
+        int zoekrij = huidigeRij;
+        return ZoekCharacterMatchInOnderliggendRij(zoekwoord, lijstMetInputRegels, zoekrij, startpositie, 0);
+    }
+
+    int ZoekCharacterMatchInOnderliggendRij(string s, List<string> lijstMetInputregels, int zoekrij, int startpositie,
+        int diagonaleAfwijking)
+    {
+        int zoekpositie = startpositie;
+        int matchAantal = 0;
+        foreach (var character in s)
+        {
+            char charInFocusVoorVergelijking = lijstMetInputregels[zoekrij][zoekpositie];
+
+            if (charInFocusVoorVergelijking.Equals(character))
+            {
+                matchAantal++;
+            }
+
+            zoekrij++;
+            zoekpositie += diagonaleAfwijking;
+            
+        }
+
+        return matchAantal;
+    }
+}
+
+void Part2(List<string> lijstMetInput)
+{
+    int antwoord = 0;
+
+    foreach (var rij in lijstMetInput)
+    {
+        for (int i = 0; i < rij.Length; i++)
+        {
+            if (rij[i].Equals('A'))
+            {
+                if (SurroundingsOK(lijstMetInput.IndexOf(rij), i, lijstMetInput))
+                {
+                    antwoord++;
+                }
+            }
+        }
+    }
+    
+    Console.WriteLine($"Day 4 - Part 2: {antwoord}"); //1965
+
+    bool SurroundingsOK(int huidigeRij, int huidigeIndex, List<string> inp )
+    {
+        bool retval;
+
+        char? linksBoven = GeefChar(huidigeRij - 1, huidigeIndex - 1, inp);
+        char? rechtsOnder = GeefChar(huidigeRij + 1, huidigeIndex + 1, inp);
+        
+        char? rechtsBoven = GeefChar(huidigeRij - 1, huidigeIndex + 1, inp);
+        char? linksOnder = GeefChar(huidigeRij + 1, huidigeIndex - 1, inp);
+        
+        if(linksBoven == null || rechtsOnder == null || rechtsBoven == null || linksOnder == null)
+        {
+            retval = false;
+        }
+        else
+        {
+            retval = (linksBoven.Equals('M') && rechtsOnder.Equals('S') ||linksBoven.Equals('S') && rechtsOnder.Equals('M') ) 
+                     && (rechtsBoven.Equals('M') && linksOnder.Equals('S') ||rechtsBoven.Equals('S') && linksOnder.Equals('M') );
         }
         
-        matchAantal = AantalMatchesVanZoekwoordDiagonaal(huidigeRij, zoekwoord, lijstMetInputRegels, startpositie, zoekrichtingRechts);
-
-        retval += matchAantal == zoekwoord.Length ? 1 : 0;
-        startpositie++;
+        return retval;
     }
-
-    return retval;
-}
-
-int AantalMatchesVanZoekwoordDiagonaal(int huidigeRij, string zoekwoord, List<string> lijstMetInputRegels, int startpositie, int zoekrichting)
-{
-    int zoekrij = huidigeRij;
-    return ZoekCharacterMatchInOnderliggendRij(zoekwoord, lijstMetInputRegels, zoekrij, startpositie, zoekrichting);
-}
-
-int AantalMatchesVanZoekwoordVerticaal(int huidigeRij, string zoekwoord, List<string> lijstMetInputRegels, int startpositie)
-{
-    int zoekrij = huidigeRij; 
-    return ZoekCharacterMatchInOnderliggendRij(zoekwoord, lijstMetInputRegels, zoekrij, startpositie,0);
-}
-
-int ZoekCharacterMatchInOnderliggendRij(string s, List<string> lijstMetInputregels, int zoekrij, int startpositie, int diagonaleAfwijking)
-{
-    int zoekpositie = startpositie;
-    int matchAantal = 0;
-    foreach (var character in s)
+    
+    char? GeefChar(int beoogdeRij, int beoogdeIndex, List<string> inputList)
     {
-        char charInFocusVoorVergelijking = lijstMetInputregels[zoekrij][zoekpositie];
-            
-        if(charInFocusVoorVergelijking.Equals(character))
+        if(beoogdeRij < 0 
+           || beoogdeRij > inputList.Count - 1 
+           || beoogdeIndex < 0 
+           || beoogdeIndex > inputList[beoogdeRij].Length - 1)
         {
-            matchAantal++;
+            return null;
         }
-            
-        zoekrij++;
-        zoekpositie += diagonaleAfwijking;
+        
+        return inputList[beoogdeRij][beoogdeIndex];
     }
-
-    return matchAantal;
 }
--- AdventOfCode/Day04/README.md
+++ AdventOfCode/Day04/README.md
@@ -56,5 +56,37 @@ drv = 1 </br>
 dra = 4 </br>
 dlv = 1 </br>
 dla = 4 </br>
-~~~~
+
 Take a look at the little Elf's word search. How many times does XMAS appear?
+
+## Part Two
+The Elf looks quizzically at you. Did you misunderstand the assignment?
+
+Looking for the instructions, you flip over the word search to find that this isn't actually 
+an XMAS puzzle; it's an X-MAS puzzle in which you're supposed to find two MAS in the 
+shape of an X. One way to achieve that is like this:
+
+`M.S`</br>
+`.A.`</br>
+`M.S`</br>
+
+Irrelevant characters have again been replaced with . in the above diagram. 
+Within the X, each MAS can be written forwards or backwards.
+
+Here's the same example from before, 
+but this time all of the X-MASes have been kept instead:
+
+`.M.S......`</br>
+`..A..MSMS.`</br>
+`.M.S.MAA..`</br>
+`..A.ASMSM.`</br>
+`.M.S.M....`</br>
+`..........`</br>
+`S.S.S.S.S.`</br>
+`.A.A.A.A..`</br>
+`M.M.M.M.M.`</br>
+`..........`</br>
+In this example, an X-MAS appears 9 times.
+
+Flip the word search from the instructions back over to the word 
+search side and try again. How many times does an X-MAS appear?
